88005a98d409a94e1f4f8096ca48e35bfeaabea1
pal73spa <pal@ngs.ru>
2021-02-08 16:56:08 +0700

    1234

---
+++ SOFT/curr_version.c
@@ -0,0 +1,8 @@
+#include "curr_version.h"
+
+const short HARDVARE_VERSION = 4;
+const short SOFT_VERSION = 5;
+const short BUILD = 2;
+const short BUILD_YEAR = 2021;
+const short BUILD_MONTH = 2;
+const short BUILD_DAY = 8;
